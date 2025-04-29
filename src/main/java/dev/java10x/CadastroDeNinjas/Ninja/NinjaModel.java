55ef121c59de156e210928b760153f9ec5c724ee
Marco Favero <marcofavero3@live.com>
2025-04-28 23:39:28 -0300

    criaçao do lombok e ajuste nas classes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Ninja;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -21,39 +27,6 @@ public class NinjaModel {
 
     // o @ faz com que um ninja tem um única missão
     @ManyToOne
+    @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
